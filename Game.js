e72e772b739f8882a85b9cf7d789a05e223c4ff2
jkmoore <jkm9@pdx.edu>
2020-05-20 16:36:19 +0900

    Implemented obstacle movement, removed the obstacle class to simplify the design

--- Game.js
+++ Game.js
@@ -1,15 +1,29 @@
+const SIZE = 180; //player and obstacle height/width
+const PX = SIZE + 20; //player position, must not draw over with obstacle image or canvas background
+
 var canvas = document.getElementById("canvas"),
   ctx = canvas.getContext("2d"),
   width = 900,
   height = 500;
 
-/*function update()
+function update()
 {
-  //Move position of obstacle o y units to the left (set obstacle image accordingly)
-  //If obstacle o is within z units of the player and is not destructible, set it to be destructible
-  //If obstacle o is destructible and we receive input in an adequate frequency/volume range for o, remove o
-  //If obstacle o overlaps with the player, end the game
-}*/
+  //If we receive audio in the adequte frequency/volume range, remove and redraw a random obstacle, reset variables as appropriate
+  //TODO
+
+
+  //Otherwise we continue to redraw the currently approaching obstacle and check if the player lost the game
+
+  ctx.fillStyle = "skyblue";
+  ctx.fillRect(PX,0,canvas.width-PX,canvas.height);
+  if (imgx > PX)
+  {
+    ctx.drawImage(img,imgx-1,imgy,SIZE,SIZE);
+    imgx = imgx - 1;
+  }
+  else
+    alert("GAME OVER");
+}
 
 canvas.width = width;
 canvas.height = height;
@@ -17,10 +31,14 @@ var img = new Image();
 
 img.onload = function()
 {
-  ctx.drawImage(img,width-200,height-200,180,180);   
+  ctx.drawImage(img,width-PX,height-PX,SIZE,SIZE);   
+  imgx = width;
+  imgy = height-PX;
 };
 img.src = "./tony.jpg";
 
+ctx.fillStyle = "red";
+ctx.fillRect(PX-SIZE,canvas.height-PX,SIZE,SIZE);
 
-//update();
+setInterval(update, 15); //TODO make this speed gradually increase, possibly with another setInterval call and another function
 
--- README.md
+++ README.md
@@ -5,11 +5,13 @@ Just gonna throw design ideas in here for now
 
 Obstacles have the following data:
   Position on a single horizontal axis (just the left edge matters because if it reaches the player the game ends)
-  Destructible or not
   Highest frequency allowed to destroy it
   Lowest frequency allowed to destroy it
   Highest volume allowed to destroy it
   Lowest volume allowed to destroy it
+
+The obstacle might not need to be an object, we can just have variables for the currently approaching obstacle and change
+them as an obstacle is destroyed and a new one approaches
 
 
 Player does not need to be an object because it's essentially a stationary point on an axis. No properties/function
@@ -17,10 +19,10 @@ Player does not need to be an object because it's essentially a stationary point
 
 The program calls an "update" function on a loop until the game ends. Looks something like this...
 Every x seconds, do the following:
-  Move position of obstacle o y units to the left
-  If obstacle o is within z units of the player and is not destructible, set it to be destructible
-  If obstacle o is destructible and we receive input in an adequate frequency/volume range for o, remove o
-  If obstacle o overlaps with the player, end the game
+  If we receive input in an adequate frequency/volume range for o, remove o and make a new obstacle
+  Else,
+    Move position of obstacle o y units to the left
+    If obstacle o overlaps with the player, end the game
 
 
 
