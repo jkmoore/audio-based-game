b4faa70aacea0f1e9c7c272bfc882c32d505c776
jkmoore <jkm9@pdx.edu>
2020-05-21 15:53:52 +0900

    Updated comments/organization of code

--- Game.js
+++ Game.js
@@ -1,14 +1,18 @@
 const SIZE = 180; //player and obstacle height/width
-const PX = SIZE + 20; //player position, must not draw over with obstacle image or canvas background
+const PX = SIZE + 20; //player position, must not draw over this position with obstacle image or canvas background
+const SPEED = 15; //milliseconds in between calls of the update() function when the game begins
 
+//Canvas-related variables
 var canvas = document.getElementById("canvas"),
   ctx = canvas.getContext("2d"),
   width = 900,
   height = 500;
 
+
+//Called repeatedly to update visuals and check for input or the game ending
 function update()
 {
-  //If we receive audio in the adequte frequency/volume range, remove and redraw a random obstacle, reset variables as appropriate
+  //If we receive audio in the adequate frequency/volume range, remove and redraw a random obstacle, reset variables as appropriate
   //TODO
 
 
@@ -25,9 +29,16 @@ function update()
     alert("GAME OVER");
 }
 
+
+/**********************************************************************************************************************************/
+
+
+//Set the canvas height and width
 canvas.width = width;
 canvas.height = height;
 
+
+//Create the obstacle image, set its properties
 var obs = new Image();
 obs.onload = function()
 {
@@ -35,10 +46,12 @@ obs.onload = function()
   obsx = width;
   obsy = height-PX;
 };
-//TODO randomly pick from an assortment of possible obstacle images, then set properties as appropriate
-//for now, just this picture of tony the tiger
+//TODO randomly pick from an assortment of possible obstacle images, then set properties as appropriate (must make additional vars)
+//For now, it's just this picture of tony the tiger and no properties because we can't receive audio input yet
 obs.src = "./tony.jpg";
 
+
+//Create the player image, set its properties
 var playerImg = new Image();
 playerImg.onload = function()
 {
@@ -46,5 +59,7 @@ playerImg.onload = function()
 }
 playerImg.src = "./player.jpeg";
 
-setInterval(update, 15); //TODO make this speed gradually increase, possibly with another setInterval call and another function
+
+//Call the update function every 15 milliseconds
+setInterval(update, SPEED); //TODO make this speed gradually increase, possibly with another setInterval call and another function
 
